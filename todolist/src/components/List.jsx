4360d101ab320ca582f9748bac1c814287a4626d
MinJae Kim <alswo4532@gmail.com>
2025-11-20 00:22:03 +0900

    수정기능 추가

--- todolist/src/components/List.jsx
+++ todolist/src/components/List.jsx
@@ -1,13 +1,13 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
 
-const List = ({ todos, onUpdate, onDelete }) => {
+const List = ({ todos, onEdit, onDelete }) => {
   return (
     <div className="List">
       <h2>오늘의 할 일</h2>
       <div className="todos_wrapper">
         {todos.map((todo) => {
-          return <TodoItem key={todo.id} {...todo} onDelete={onDelete} />;
+          return <TodoItem key={todo.id} {...todo} onDelete={onDelete} onEdit={onEdit} />;
         })}
       </div>
     </div>
--- todolist/src/components/TodoItem.jsx
+++ todolist/src/components/TodoItem.jsx
@@ -1,14 +1,42 @@
+import { useState } from "react";
 import "./TodoItem.css";
 
-const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
+const TodoItem = ({ id, content, date, onEdit, onDelete }) => {
   const onClickDelete = () => {
     onDelete(id);
   };
+
+  const [isEdit, setIsEdit] = useState(false);
+  const [localContent, setLocalContent] = useState(content);
+
+  const handleEdit = () => {
+    setIsEdit(true);
+  };
+  const handleCancel = () => {
+    setIsEdit(false);
+    setLocalContent(content);
+  };
+  const handleSave = () => {
+    onEdit(id, localContent);
+    setIsEdit(false);
+  };
+
   return (
     <div className="TodoItem">
-      <div className="tit">{content}</div>
-      <div className="date">{new Date(date).toLocaleDateString()}</div>
-      <button onClick={onClickDelete}>삭제</button>
+      {isEdit ? (
+        <>
+          <input type="text" value={localContent} onChange={(e) => setLocalContent(e.target.value)} />
+          <button onClick={handleSave}>완료</button>
+          <button onClick={handleCancel}>취소</button>
+        </>
+      ) : (
+        <>
+          <div className="tit">{content}</div>
+          <div className="date">{new Date(date).toLocaleDateString()}</div>
+          <button onClick={handleEdit}>수정</button>
+          <button onClick={onClickDelete}>삭제</button>
+        </>
+      )}
     </div>
   );
 };
